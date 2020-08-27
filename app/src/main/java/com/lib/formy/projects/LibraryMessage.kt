542f5f3b03fdea9d44c4bbcc12038dd4d8605bb3
Андрей Николаенко <andriy.nikolaenko1012@gmail.com>
2020-08-27 15:19:21 +0300

    update to 1.2.0, added volley

--- app/src/main/java/com/lib/formy/projects/LibraryMessage.kt
+++ app/src/main/java/com/lib/formy/projects/LibraryMessage.kt
@@ -1,29 +1,45 @@
 package com.lib.formy.projects
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import androidx.browser.customtabs.CustomTabsIntent
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import java.net.URL
 import java.util.concurrent.Executors
 
-class LibraryMessage(private var firstActivity: Activity, private var secondActivity: Activity, private var content: String?) {
+class LibraryMessage(private var ctx: Context, private var firstActivity: Activity, private var secondActivity: Activity, private var content: String?) {
     fun init(s: String){
         if(InternetConnection.checkConnection(firstActivity)){
             if (content != null){
-                Executors.newSingleThreadExecutor().execute{
-                    val json = URL("${Utils().decodeString(s)}?$content").readText()
-                    val convertedObject = Gson().fromJson(json, MyResponse::class.java)
-                    if (convertedObject.data != null){
-                        s(convertedObject.data!!, content!!)
-                    } else {
+                val queue = Volley.newRequestQueue(ctx)
+                val url = "${Utils().decodeString(s)}?$content"
+                val stringRequest = StringRequest(
+                    Request.Method.GET, url,
+                    Response.Listener { response ->
+                        val convertedObject = Gson().fromJson(response, MyResponse::class.java)
+                        if (convertedObject != null){
+                            if (convertedObject.data != null){
+                                s(convertedObject.data!!, content!!)
+                            } else {
+                                finish()
+                            }
+                        } else {
+                            finish()
+                        }
+                    },
+                    Response.ErrorListener {
                         finish()
-                    }
-                }
+                    })
+                queue.add(stringRequest)
             } else {
                 finish()
             }
